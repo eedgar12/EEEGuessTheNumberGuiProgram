d12173cdb287e343fe6ddea97d70efe944b1af95
Toshiba laptop <Toshiba laptop@ElmoToshiba>
2012-11-07 21:29:05 -0600

    First commit.  Program works in GUI class only.

---
+++ src/guessthenumberguiprogram/NumberGuessService.java
@@ -0,0 +1,26 @@
+package guessthenumberguiprogram;
+
+/**
+ *
+ * @author Emma Edgar
+ */
+public class NumberGuessService {
+
+    private enum guessResult {HIGH, LOW, CORRECT};
+    private double guess;
+    
+    public double hi;
+    
+//    public String resultString(double num){
+//        if (num == guess) {
+//            return ("You guessed correctly");
+//        } else if (num.compareTo(guess) == -1) {
+//            return ("You guessed high");
+//        } else if (num.compareTo(guess) == 1) {
+//            return ("You guessed low");
+//        } else {
+//            lblResults.setText("Sorry, there was an error in the program");
+//        }
+//    }
+            
+}
---
+++ src/guessthenumberguiprogram/Startup.java
@@ -0,0 +1,13 @@
+package guessthenumberguiprogram;
+
+public class Startup {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
